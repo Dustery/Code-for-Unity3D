031031141b1ec5a2ac034326f122c5389e42f3d1
Oleksandr Kostynskyi <kostynskyi95@gmail.com>
2019-11-28 18:26:16 +0100

    Commit
    finish.cs - the object to which players touches and the game will end with the victory of the one who first came to this object.
    MovingPlayer.cs - move the player with the arrow keys on the keyboard.

---
+++ MovingPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlayer : MonoBehaviour
+{
+    
+    public GameObject player1;
+    public float range = 1f, moveSpeed = 5f, turnSpeed = 25f;
+   
+    void Start()
+    {
+        player1 = (GameObject)this.gameObject;
+    }
+
+    
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            player1.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            player1.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            player1.transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            player1.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            
+
+
+}
+
+}
---
+++ finish.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.UI;
+
+public class finish : MonoBehaviour
+{  
+    [SerializeField]Collider player1;
+    [SerializeField]Collider player2;
+   // [SerializeField]Text win;
+    private bool winPlace = false;
+    private string winner; 
+
+
+    void OnGUI()
+    {
+        if (winPlace)
+        {
+            int w = Screen.width, h = Screen.height;
+
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(0, 0, w, h);
+            style.alignment = TextAnchor.MiddleCenter;
+            style.fontSize = h * 2 / 50;
+            style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            string text = string.Format("Great! " + winner + " win!");
+            GUI.Label(rect, text, style);
+
+            
+        }
+    }  
+
+    public void OnTriggerEnter(Collider col){ 
+        winPlace = true;
+        
+        if(col.name == "player1"){
+            Debug.Log("Player 1 won!");
+            winner ="Green player";
+        }
+            else if(col.name == "player2"){
+            Debug.Log("Player 2 won!");
+            winner ="Red player";
+        }
+    }
+
+}
